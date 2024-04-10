8189b627c9d9366851efaf80dd326c7b631e0497
ituq <vincentluisfranz@gmail.com>
2024-04-10 15:35:55 +0200

    moved website to app

--- app/app.py
+++ app/app.py
@@ -29,4 +29,4 @@ def result():
 def custom_static(filename):
     return send_from_directory('static/graphs', filename)
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=80)
